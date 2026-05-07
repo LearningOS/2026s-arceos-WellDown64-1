40ec993fc4e3a8c50cabeb62bd102e2182cb068c
welldown <hangw7252@gmail.com>
2026-05-07 12:39:02 +0800

    complete exercise: print_with_color

--- arceos/ulib/axstd/src/io/stdio.rs
+++ arceos/ulib/axstd/src/io/stdio.rs
@@ -166,8 +166,8 @@ pub fn __print_impl(args: core::fmt::Arguments) {
     if cfg!(feature = "smp") {
         // synchronize using the lock in axlog, to avoid interleaving
         // with kernel logs
-        arceos_api::stdio::ax_console_write_fmt(args).unwrap();
+        arceos_api::stdio::ax_console_write_fmt(format_args!("\x1b[34m{}\x1b[0m", args)).unwrap();
     } else {
-        stdout().lock().write_fmt(args).unwrap();
+        stdout().lock().write_fmt(format_args!("\x1b[34m{}\x1b[0m", args)).unwrap();
     }
 }
